929618eed574acb7802e589b6fa1d73d2c12ed53
SchmidtAlejo <schmidtalejo@gmail.com>
2022-05-20 20:14:57 -0300

    Schmidt Alejo Gabriel

--- controllers/sales.js
+++ controllers/sales.js
@@ -4,4 +4,24 @@ async function getSales(){
     return sales.getAllSales();
 }
 
-module.exports = {getSales};
+async function getSale(id){    
+    return sales.getSale(id);
+}
+
+async function getSaleByPurchaseMethod(Query){
+    return sales.getSaleByPurchaseMethod(Query);
+}
+
+async function getSaleByEmail(email){
+    return sales.getSaleByEmail(email);
+}
+
+async function getSaleBySatisfaction(satisfaction){
+    return sales.getSaleBySatisfaction(satisfaction);
+}
+
+async function getTotalPerLocation(location){
+    return sales.getTotalPerLocation(location);
+}
+
+module.exports = {getSales, getSale, getSaleByPurchaseMethod, getSaleByEmail, getSaleBySatisfaction, getTotalPerLocation};
--- routes/sales.js
+++ routes/sales.js
@@ -4,7 +4,29 @@ const controller = require('../controllers/sales');
 
 router.get('/', async (req, res) => {
     console.log("check");
-    res.json(await controller.getSales());
+    if(req.query.purchaseMethod){
+        res.json(await controller.getSaleByPurchaseMethod(req.query.purchaseMethod));
+    }
+    else if(req.query.email){
+        res.json(await controller.getSaleByEmail(req.query.email));
+    }
+    else if(req.query.location){
+        const TOTAL= await controller.getTotalPerLocation(req.query.location)
+        res.send(`El importe total de ${req.query.location} es de ${TOTAL}`);
+    }
+    else{
+        res.json(await controller.getSales());
+    }
+});
+
+router.get('/lowSatisfaction', async (req, res) => {
+    console.log("check");
+    res.json(await controller.getSaleBySatisfaction(req.query.satisfaction));
+});
+
+router.get('/:id', async (req, res) => {
+    console.log("check");
+    res.json(await controller.getSale(req.params.id));
 });
 
 module.exports = router;
