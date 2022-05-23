de7d84e07d86486bed7e1b41d6b9790db6d8baf8
Cecilia Tarsia <ceciliatarsia@gmail.com>
2022-05-23 01:17:19 -0300

    resolucion TARSIA, MARIA CECILIA

--- controllers/sales.js
+++ controllers/sales.js
@@ -1,7 +1,40 @@
-const sales = require('../data/supplies');
+const sales = require("../data/supplies");
 
-async function getSales(){    
-    return sales.getAllSales();
+async function getSales() {
+  return sales.getAllSales();
 }
 
-module.exports = {getSales};
+async function getSale(id) {
+  return sales.getSale(id);
+}
+
+async function getSalesByPurchaseMethod(pm) {
+  return sales.getSalesByPurchaseMethod(pm);
+}
+
+async function getSalesByEmail(email) {
+  return sales.getSalesByEmail(email);
+}
+
+async function getUnsatisfiedCustomers() {
+  return sales.getUnsatisfiedCustomers();
+}
+
+// async function getUnsatisfiedCustomersWithParams(num) {
+//   return sales.getUnsatisfiedCustomersWithParams(num);
+// }
+
+// NO FUNCIONA
+async function getImporteTotal(location) {
+  return sales.getImporteTotal(location);
+}
+
+module.exports = {
+  getSales,
+  getSale,
+  getSalesByPurchaseMethod,
+  getSalesByEmail,
+  getUnsatisfiedCustomers,
+  // getUnsatisfiedCustomersWithParams,
+  getImporteTotal,
+};
--- routes/sales.js
+++ routes/sales.js
@@ -1,10 +1,51 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const controller = require('../controllers/sales');
+const controller = require("../controllers/sales");
 
-router.get('/', async (req, res) => {
-    console.log("check");
-    res.json(await controller.getSales());
+router.get("/", async (req, res) => {
+  console.log("check");
+  res.json(await controller.getSales());
+});
+
+//1. Necesitamos un endpoint que nos devuelva una venta particular por _id
+router.get("/:id", async (req, res) => {
+  const sale = await controller.getSale(req.params.id);
+  res.json(sale);
+});
+
+//2. Necesitamos un endpoint que nos permita listar las ventas filtradas por el metodo de compra (purchaseMethod),
+//   que pueden ser: Phone, Online, In store...
+router.get("/purchaseMethod/:purchaseMethod", async (req, res) => {
+  const sales = await controller.getSalesByPurchaseMethod(
+    req.params.purchaseMethod
+  );
+  res.json(sales);
+});
+
+//3. Necesitamos un endpoint que nos devuelva las compras de un cliente customer por email
+router.get("/byEmail/:email", async (req, res) => {
+  const sales = await controller.getSalesByEmail(req.params.email);
+  res.json(sales);
+});
+
+//4. Necesitamos encontrar los clientes insatisfechos (con menor a 3 de satisfacción) VERSION OK
+router.get("/unsatisfied", async (req, res) => {
+  const customers = await controller.getUnsatisfiedCustomers();
+  res.json(customers);
+});
+
+//4. Necesitamos encontrar los clientes insatisfechos (con menor a 3 de satisfacción) //NO FUNCIONA
+// router.get("/unsatisfied/:satisfaction", async (req, res) => {
+//   const customers = await controller.getUnsatisfiedCustomersWithParams(
+//     req.params.satisfaction
+//   );
+//   res.json(customers);
+// });
+
+//5. Generar un endpoint para obtener el importe total de la venta por localizacion // NO FUNCIONA
+router.get("/importeTotal/:storeLocation", async (req, res) => {
+  const total = await controller.getImporteTotal(req.params.storeLocation);
+  res.json(total);
 });
 
 module.exports = router;
